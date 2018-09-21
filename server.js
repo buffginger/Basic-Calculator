3501d347a7e5009be2a5c1c688a4a9a3f2082ee0
Ethan Anderson <andersonethan683@gmail.com>
2018-09-20 21:22:30 -0500

    Added type checking to only accept numbers

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require('express');
 
 var app = express();
 
+var nan = 'Sorry, please enter two integers.';
 
 
 app.get('/', (req, res) => {
 	var msg = 'Welcome to the Basic Calculator! There are four endpoints'
 	+ ' available to use: add, subtract, multiply, divide.' 
-	+ ' To use one, simply navigate to http://127.0.0.1:8081/method/number1/number2'
+	+ ' To use one, simply navigate to http://127.0.0.1:8081/method/number1/number2.'
 	+' For example: http://127.0.0.1:8081/add/3/4';
 	console.log(msg);
 	res.send(msg)
@@ -16,43 +17,69 @@ app.get('/', (req, res) => {
 app.get('/add/:number1/:number2', (req, res) => {
 	var num1 = parseFloat(req.params.number1);
 	var num2 = parseFloat(req.params.number2);
-	var sum = num1 + num2;
-	var result = `${num1} + ${num2} = ${sum}`
+	// Verify integer values
+	if (!isNaN(num1) && !isNaN(num2)) {
+		var sum = num1 + num2;
+		var result = `${num1} + ${num2} = ${sum}`
+		// Output result to the terminal and browser
+		console.log(result);
+		res.send(result)
 
-	console.log(result);
-	res.send(result)
+	} else {
+		console.log(nan);
+		res.send(nan);
+	}
 })
 
 app.get('/subtract/:number1/:number2', (req, res) => {
 	var num1 = parseFloat(req.params.number1);
 	var num2 = parseFloat(req.params.number2);
-	var dif = num1 - num2;
-	var result = `${num1} - ${num2} = ${dif}`
+	// Verify integer values
+	if (!isNaN(num1) && !isNaN(num2)) {
+		var dif = num1 - num2;
+		var result = `${num1} - ${num2} = ${dif}`
+		// Output result to the terminal and browser
+		console.log(result);
+		res.send(result)
 
-	console.log(result);
-	res.send(result)
+	} else {
+		console.log(nan);
+		res.send(nan);
+	}
 })
 
 app.get('/multiply/:number1/:number2', (req, res) => {
 	var num1 = parseFloat(req.params.number1);
 	var num2 = parseFloat(req.params.number2);
-	var product = num1 * num2;
-	var result = `${num1} * ${num2} = ${product}`
+	// Verify integer values
+	if (!isNaN(num1) && !isNaN(num2)) {
+		var product = num1 * num2;
+		var result = `${num1} * ${num2} = ${product}`
+		// Output result to the terminal and browser
+		console.log(result);
+		res.send(result)
 
-	console.log(result);
-	res.send(result)
-	res.end
+	} else {
+		console.log(nan);
+		res.send(nan);
+	}
 })
 
 app.get('/divide/:number1/:number2', (req, res) => {
 	var num1 = parseFloat(req.params.number1);
 	var num2 = parseFloat(req.params.number2);
-	var quotient = num1 / num2;
-	var result = `${num1} / ${num2} = ${quotient}`
+	// Verify integer values
+	if (!isNaN(num1) && !isNaN(num2)) {
+		var quotient = num1 / num2;
+		var result = `${num1} / ${num2} = ${quotient}`
+		// Output result to the terminal and browser
+		console.log(result);
+		res.send(result)
 
-	console.log(result);
-	res.send(result)
-	res.end
+	} else {
+		console.log(nan);
+		res.send(nan);
+	}
 })
 var server = app.listen(8081, function () {
 
